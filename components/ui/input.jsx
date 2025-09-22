a67e1580bdd8484dc3fc91eb03e1ba987bac5292
SketchMorph <msp3692a@gmail.com>
2025-09-23 00:29:45 +0900

    Update input.jsx

--- components/ui/input.jsx
+++ components/ui/input.jsx
@@ -1,14 +1,10 @@
 "use client";
 import React from "react";
-import clsx from "clsx";
 
-export function Input({ className, ...props }) {
+export function Input({ className = "", ...props }) {
   return (
     <input
-      className={clsx(
-        "px-4 py-2 rounded-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 w-full",
-        className
-      )}
+      className={`px-4 py-2 rounded-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 w-full ${className}`}
       {...props}
     />
   );
